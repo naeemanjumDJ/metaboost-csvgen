e354f579914d3926adc1c8046c3e04e973246bba
thisnaeem <naeemanjum076@gmail.com>
2025-02-08 00:44:57 +0500

    ok

--- app/(pages)/(protected)/dashboard/tasks/page.tsx
+++ app/(pages)/(protected)/dashboard/tasks/page.tsx
@@ -1,1 +1,15 @@
-// Delete this entire file
+"use client";
+
+import React from "react";
+import TasksTable from "@/components/admin/TasksTable";
+
+const TasksPage: React.FC = () => {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-5xl font-semibold">Tasks</h1>
+      <TasksTable isAdmin={false} />
+    </div>
+  );
+};
+
+export default TasksPage as unknown as typeof TasksPage;
